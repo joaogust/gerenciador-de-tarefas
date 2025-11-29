0b4e761752b1704315b1fa2d981daea31ccc16f9
Elton Araujo <eltonlimaaraujo@outlook.com>
2025-11-29 11:21:31 -0300

    Camada Model e mapeamento das entidades

--- kanban/src/main/java/br/edu/ifsp/kanban/model/Tarefa.java
+++ kanban/src/main/java/br/edu/ifsp/kanban/model/Tarefa.java
@@ -1,4 +1,70 @@
 package br.edu.ifsp.kanban.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tarefa")
 public class Tarefa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_tarefa")
+    private Integer idTarefa;
+
+    @Column(name = "texto", nullable = false)
+    private String texto;
+    @Column(name = "estado", nullable = false)
+    private boolean estado;
+
+    // Relacionamentos
+    @Column(name = "id_bloco", insertable = false, updatable = false)
+    private Integer idBloco;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_bloco", nullable = false)
+    private Bloco bloco;
+
+    // Construtor vazio
+    public Tarefa() {}
+
+    // Getters e Setters
+
+    public Integer getIdTarefa() {
+        return idTarefa;
+    }
+
+    public void setIdTarefa(Integer idTarefa) {
+        this.idTarefa = idTarefa;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public Integer getIdBloco() {
+        return idBloco;
+    }
+
+    public void setIdBloco(Integer idBloco) {
+        this.idBloco = idBloco;
+    }
+
+    public Bloco getBloco() {
+        return bloco;
+    }
+
+    public void setBloco(Bloco bloco) {
+        this.bloco = bloco;
+    }
 }
--- kanban/src/main/java/br/edu/ifsp/kanban/model/Usuario.java
+++ kanban/src/main/java/br/edu/ifsp/kanban/model/Usuario.java
@@ -1,4 +1,71 @@
 package br.edu.ifsp.kanban.model;
 
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "usuario")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Integer idUsuario;
+
+    @Column(name = "nome")
+    private String nome;
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+    @Column(name = "senha", nullable = false)
+    private String senha;
+
+    // Relacionamento com PaginaUsuario
+    @OneToMany(mappedBy = "usuario")
+    private Set<PaginaUsuario> paginas = new HashSet<>();
+
+    // Construtor vazio
+    public Usuario() {}
+
+    // Getters e Setters
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Set<PaginaUsuario> getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(Set<PaginaUsuario> paginas) {
+        this.paginas = paginas;
+    }
 }
